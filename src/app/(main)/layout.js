e999cfdb23ca21475a5d3b5374ee6695fd19662f
azizbeAnvarjanov <azizbeanvarjanov@gmail.com>
2025-06-13 21:21:08 +0500

    remove notification

--- src/app/(main)/layout.js
+++ src/app/(main)/layout.js
@@ -26,7 +26,6 @@ export default function RootLayout({ children }) {
       >
         <StudentProvider>
           <Navbar />
-          <NotificationPermission />
           <main className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8 pt-20">
             {children}
           </main>
